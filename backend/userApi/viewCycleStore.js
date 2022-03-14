c49898569ef2beafb3659504f7f3b44cc00472f2
Arpit Kumar Rai <arpitkr20@iitk.ac.in>
2022-03-14 20:38:24 +0530

    Modified view cycle store to also return data of all cycles

--- backend/userApi/viewCycleStore.js
+++ backend/userApi/viewCycleStore.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import dealerModel from '../schema/dealerSchema.js';
+import availableCycle from './helperFunctions/availableCycle.js';
+import availableCycleAll from './helperFunctions/availableCycle.js';
 
 
 //Link with mongodb server using mongoose
@@ -12,16 +14,83 @@ async function main() {
 
 // Accepts a request object {dealerId: mongoose.Types.ObjectId, cycleStoreId:mongoose.Types.ObjectId}. If set, returns the cycle store data corresponding to that cycle store else
 //returns data of all stores.
-// Returns an array of objects where each object is of the form {_id: , cycleStoreData: } where _id is default dealerId in dealerSchema
+
+
+// Returns an object with keys as cycleStoreId :
+// {
+    // cycleStoreId1: {
+    //     dealerId:
+    //     dealerName:
+    //     dealerAddress:
+    //     dealerContact:
+    //     dealerEmail:
+    //     cycleStoreAddress:
+    //     cycleStoreContact: 
+    //     cycles: {
+    //         cycleId1: {
+    //             name:
+    //             rate:
+    //             availableCycle:
+    //         },
+    //         cycleId2: {
+
+    //         }
+    //     }
+    // }
+// }
 
 async function viewCycleStore(req,res){
 
     if(!req.dealerId){
-        const cycleStoreData = await dealerModel.find({},'_id cycleStore');
-        return res.status(200).json(cycleStoreData);
-    }else{
-        const cycleStoreData = await dealerModel.find({_id:req.dealerId,"cycleStore.cycleStoreId":req.cycleStoreId},'_id cycleStore');
-        return res.status(200).json(cycleStoreData);
+        const dealerData = await dealerModel.find({});
+        
+        const allAvailableCycle = await availableCycleAll();
+
+        let allData = {};
+
+        dealerData.forEach(dealer => {
+            const dealerId = dealer._id;
+
+
+            dealer.cycleStore.forEach(cycleStore => {
+                const cycleStoreId = cycleStore.cycleStoreId;
+
+                let cycleObject = {};
+
+                cycleStore.cycles.forEach(cycle => {
+
+                    cycleObject[cycle.cycleId] = {
+                        name: cycle.name,
+                        rate: cycle.rate,
+                        availableCycle: allAvailableCycle[dealerId][cycleStoreId][cycle.cycleId]
+                    }
+
+                })
+
+                allData[cycleStoreId] = {
+                    dealerId: dealerId,
+                    dealerName: dealer.name,
+                    dealerAddress: dealer.address,
+                    dealerContact: dealer.contact,
+                    dealerEmail: dealer.email,
+                    cycleStoreAddress: cycleStore.address,
+                    cycleStoreContact: cycleStore.contact,
+                    cycles: cycleObject
+                }
+
+            })
+
+        })
+
+
+        return res.status(200).json(allData);
+
     }
+    // else{
+    //     const cycleStoreData = await dealerModel.find({_id:req.dealerId,"cycleStore.cycleStoreId":req.cycleStoreId},'_id cycleStore');
+    //     return res.status(200).json(cycleStoreData);
+    // }
 
 }
+
+export default viewCycleStore;
