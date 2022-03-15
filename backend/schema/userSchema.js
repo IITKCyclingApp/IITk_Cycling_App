e9d4c1f82f73c11d83cd312b9df0d69a0178226c
Arpit Kumar Rai <arpitkr20@iitk.ac.in>
2022-03-15 11:04:00 +0530

    Add userId field

--- backend/schema/userSchema.js
+++ backend/schema/userSchema.js
@@ -2,11 +2,17 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+    userId: mongoose.Types.ObjectId,
     name: String,
     address: String,
     contact: [Number],
-    email: String,
-    roll: Number,
+    favourites: [{
+        dealerId: mongoose.Types.ObjectId,
+        cycleStoreId: mongoose.Types.ObjectId,
+        cycleId: mongoose.Types.ObjectId
+    }],
+    email: {type:String, unique:true} ,
+    roll: {type:Number, unique:true},
     username: String
 })
 
