aabf17d00fcfd37ff975ec2a71e6328413853902
Umam27 <umam20@iitk.ac.in>
2022-03-17 10:30:56 +0530

    info-page-add-login-page-add

---
+++ frontend/cycle_app/src/App.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+// import InfoPage from './pages/info'
+// import LoginPage from './pages/login'
+// import RegisterPage from './pages/register'
+
+// import './App.css'
+
+// export default function App() {
+//     return (
+//         <Router>
+//             <div>
+//                 <Switch>
+                  
+//                     <Route exact path="/" component={ InfoPage } />
+//                     <Route path="/login" component={ LoginPage } />
+//                     <Route path="/register" component={ RegisterPage } />
+
+//                 </Switch>
+//             </div>
+//         </Router>
+//     )
+// }
---
+++ frontend/cycle_app/src/signin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signin = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default signin
