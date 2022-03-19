1cfd657fed0b5aa4aff6a4abd1e22a796887042e
Het Patel <hetpatel20@iitk.ac.in>
2022-03-19 14:59:42 +0530

    Add user_end Component

---
+++ frontend/cycle_app/src/Component/cycleStore.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import cycleTile from './cycleTile';
+
+class cycleStore extends React.Component{
+    render(){
+        return (
+            <div className="Cycle Store">
+                <h4> Cycles</h4>
+            {this.props.cycles.map((cycle)=>{
+            return <cycleTile cycle={cycle}/>
+            })
+            }
+            </div>
+        )
+}
+}
+export default cycleStore;
---
+++ frontend/cycle_app/src/Component/cycleTile.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class cycleTile extends React.Component{
+    render()
+    {
+        return(
+        <div className="col-md-4 col-sm-6 col-xs-12">
+  <div className="featured-item">
+    <div className="thumb">
+      <div className="thumb-img">
+        <img src="https://source.unsplash.com/random/720×480/?fruit" alt />
+      </div>
+      {/* Two styles of add to favourites button */}
+      {/* <div class="plus-button overlay-content">
+                              <a href="team.html"><i class="fa fa-plus"></i></a>
+                          </div> */}
+      <div className="overlay-content">
+        <button style={{"background-color":"Orange","color":"white"}}>Add to Favourites</button>
+      </div>
+    </div>
+    <div className="down-content">
+      <h4 id="cycleName">Cycle Name : {this.props.cycle.name}</h4>
+      <h4 id="cycleRate">Cycle Rate : {this.props.cycle.rate}</h4>
+      <h4 id="dealerNumber">Dealer Contact Number : {this.props.cycle.contact_number}</h4>
+      <h4 id="bookingTime">Booking Time : {this.props.cycle.booking_time}</h4>
+      <br />
+      <div className="text-button">
+        <a href="#"><strong>Rent</strong></a>
+      </div>
+    </div>
+  </div>
+</div>
+        )
+    }
+}
+
+export default cycleTile;
